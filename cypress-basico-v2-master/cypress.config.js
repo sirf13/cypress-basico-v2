438b05ec8c0ae6d1e38f3c9033d46110b2d05b86
sirf13 <thiagosouzaf13@hotmail.com>
2024-11-19 18:22:05 -0300

    versão

--- cypress-basico-v2-master/cypress.config.js
+++ cypress-basico-v2-master/cypress.config.js
@@ -1,15 +1,20 @@
-const { defineConfig } = require('cypress')
+const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  
   integration: {
     viewportHeight: 880,
     viewportWidth: 1280,
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
     setupNodeEvents(on, config) {
-      return require('./cypress/plugins/index.js')(on, config)
+      return require("./cypress/plugins/index.js")(on, config);
     },
-    specPattern: 'cypress/integration/CAC-TAT.cy.js'
+    specPattern: "cypress/integration/CAC-TAT.cy.js",
   },
-})
+
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
