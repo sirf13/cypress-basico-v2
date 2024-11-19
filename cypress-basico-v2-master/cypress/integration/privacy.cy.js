bbc3b52d39aea0677efbc221e7a0ab7b785c82f5
sirf13 <thiagosouzaf13@hotmail.com>
2024-11-19 19:15:50 -0300

    Acabou🙏

--- cypress-basico-v2-master/cypress/integration/privacy.cy.js
+++ cypress-basico-v2-master/cypress/integration/privacy.cy.js
@@ -1,5 +1,8 @@
 //modulo8
-it('testa a página da política de privacidade de forma independente', function () {
-    cy.visit('./src/privacy.html')
-    cy.contains('CAC TAT - Política de privacidade').should('be.visible')
+Cypress._.times(20, function(){
+    it('testa a página da política de privacidade de forma independente', function () {
+        cy.visit('./src/privacy.html')
+        cy.contains('CAC TAT - Política de privacidade').should('be.visible')
+    })
+    
 })
